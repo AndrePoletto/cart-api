dadac3527236e3912b391448c1e231eeb337f696
André Poletto <andre.poletto@omeletecompany.com>
2021-04-16 01:13:40 -0300

    Creating a cart model

---
+++ api/models/cartModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+
+const CartSchema = mongoose.Schema({
+    cartId: { type: String, unique: true, required: true },
+    customer: {
+        customerId: String,
+        email: String
+    },
+    items: [{
+        itemId: String,
+        quantity: Number,
+        price: Number,
+        imageUrl: String,
+        uri: String,
+        productName: String,
+        productCode: String
+    }],
+    coupon: [{
+        couponId: String,
+        couponName: String,
+        couponValue: Number
+    }]
+}, {timestamps: true, versionKey: false});
+
+module.exports = mongoose.model('Cart', CartSchema);
--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 const config = {
-    MONGO_URI: process.env.MONGO_URI || "mongodb://challenge-dev:p4ssw0rd@0.0.0.0:27017",
+    MONGO_URI: process.env.MONGO_URI || "mongodb://challenge-dev:p4ssw0rd@0.0.0.0:27017/ecommerce",
     PUBLIC_PORT: process.env.PUBLIC_PORT || 8080
 };
 
